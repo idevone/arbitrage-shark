0070c358b9cd7f2a56e45580c6d9645af99b720e
dkarp <dkarputs@gmail.com>
2024-08-19 01:46:40 +0200

    Event send to pixel fix

--- crm/views/redirect/index.php
+++ crm/views/redirect/index.php
@@ -14,8 +14,8 @@
         t.src=v;s=b.getElementsByTagName(e)[0];
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '<?= $pixel_id ?>'); // Здесь PHP переменная используется для замены на ваш Pixel ID
-    fbq('track', 'PageView');
+    fbq('init', '<?= $pixel_id ?>');
+    fbq('track', 'Subscribe');
 
     window.location.href = 'tg://join?invite=<?= $invite_link ?>';
 </script>
