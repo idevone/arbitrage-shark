1fa3e9ed8d348ac42de48e127e8fd9dd2a5dd26c
dkarp <dkarputs@gmail.com>
2024-08-19 03:02:39 +0200

    Change event name

--- crm/views/redirect/index.php
+++ crm/views/redirect/index.php
@@ -15,7 +15,7 @@
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '<?= $pixel_id ?>');
-    fbq('track', 'PageView');
+    fbq('track', 'Subscribe');
 
     window.location.href = 'tg://join?invite=<?= $invite_link ?>';
 </script>
