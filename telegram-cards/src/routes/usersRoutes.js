f60cbc2aa1fb0c693591282df016fee84bd4dbf3
dkarp <dkarputs@gmail.com>
2024-08-23 20:08:03 +0200

    Telegram cards folder deleted

--- telegram-cards/src/routes/usersRoutes.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const { register } = require("../controllers/usersControllers");
-const router = new Router();
-
-router.post("/register", register);
-
-module.exports = router;
