42ab46d374e800f386a375fe68a137ee417e9537
Dmitriy <156363280+dkarputs@users.noreply.github.com>
2024-09-16 18:02:05 +0200

    Update db.php

--- crm/config/db.php
+++ crm/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=vpn-vds113.eleos.tk;port=5432;dbname=postgres',
-    'username' => 'postgres',
-    'password' => 'postgres',
+    'dsn' => 'pgsql:host=vultr-prod-47ece5d5-eb9c-4855-94c5-8804581daa12-vultr-prod-92d7.vultrdb.com;port=16751;dbname=audience-postgres',
+    'username' => 'vultradmin',
+    'password' => 'AVNS_nLThkCM8QhWbexMvWAk',
     'charset' => 'utf8',
 ];
