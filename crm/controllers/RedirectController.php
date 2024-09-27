38f41577529d3d92f46656f0bdc4f276a0bf016e
dkarputs <dkarputs@gmail.com>
2024-09-27 16:44:56 +0200

    fix referer getting

--- crm/controllers/RedirectController.php
+++ crm/controllers/RedirectController.php
@@ -28,41 +28,7 @@ class RedirectController extends Controller
                 $invite_link = explode('+', $invite_link)[1];
 
                 $pixel_id = Yii::$app->request->get('pixel', '');
-                $pixel_api = \app\models\Pixel::find()->select('pixel_api')->where(['pixel_id' => $pixel_id])->scalar();
-                $campaign_id = Yii::$app->request->get('campaign_id', '');
-                $ad_id = Yii::$app->request->get('ad_id', '');
-                $campaign_name = Yii::$app->request->get('campaign_name', '');
-                $adset_name = Yii::$app->request->get('adset_name', '');
-                $adset_id = Yii::$app->request->get('adset_id', '');
-                $ad_name = Yii::$app->request->get('ad_name', '');
-                $placement = Yii::$app->request->get('placement', '');
-                $site_source_name = Yii::$app->request->get('site_source_name', '');
-                $fbclid = Yii::$app->request->get('fbclid', '');
-                $gclid = Yii::$app->request->get('gclid', '');
-                $invite_code = $invite_link;
-                $client_ip_address = $_SERVER['REMOTE_ADDR'];
-                $client_user_agent = $_SERVER['HTTP_USER_AGENT'];
-                // $refer = "Direct";
 
-                Yii::$app->db->createCommand()->insert('audience', [
-                    'pixel_id' => $pixel_id,
-                    'campaign_id' => $campaign_id,
-                    'ad_id' => $ad_id,
-                    'campaign_name' => $campaign_name,
-                    'adset_name' => $adset_name,
-                    'adset_id' => $adset_id,
-                    'ad_name' => $ad_name,
-                    'placement' => $placement,
-                    'site_source_name' => $site_source_name,
-                    'status' => 'PageView',
-                    'fbclid' => $fbclid,
-                    'invite_code' => $invite_code,
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'client_ip_address' => $client_ip_address,
-                    'client_user_agent' => $client_user_agent,
-                    // 'refer' => $refer,
-                    'gclid' => $gclid,
-                ])->execute();
 
                 return $this->render('index', ['id' => $id, 'invite_link' => $invite_link, 'pixel_id' => $pixel_id]);
             } else {
--- crm/views/redirect/index.php
+++ crm/views/redirect/index.php
@@ -6,10 +6,43 @@
 
 $this->title = 'Redirecting...';
 
-$client_referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Direct';
+$pixel_id = Yii::$app->request->get('pixel', '');
+$pixel_api = \app\models\Pixel::find()->select('pixel_api')->where(['pixel_id' => $pixel_id])->scalar();
+$campaign_id = Yii::$app->request->get('campaign_id', '');
+$ad_id = Yii::$app->request->get('ad_id', '');
+$campaign_name = Yii::$app->request->get('campaign_name', '');
+$adset_name = Yii::$app->request->get('adset_name', '');
+$adset_id = Yii::$app->request->get('adset_id', '');
+$ad_name = Yii::$app->request->get('ad_name', '');
+$placement = Yii::$app->request->get('placement', '');
+$site_source_name = Yii::$app->request->get('site_source_name', '');
+$fbclid = Yii::$app->request->get('fbclid', '');
+$gclid = Yii::$app->request->get('gclid', '');
+$invite_code = $invite_link;
+$client_ip_address = $_SERVER['REMOTE_ADDR'];
+$client_user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+$client_referer = Yii::$app->request->get('referer', 'Direct');
+
 
 Yii::$app->db->createCommand()->insert('audience', [
+    'pixel_id' => $pixel_id,
+    'campaign_id' => $campaign_id,
+    'ad_id' => $ad_id,
+    'campaign_name' => $campaign_name,
+    'adset_name' => $adset_name,
+    'adset_id' => $adset_id,
+    'ad_name' => $ad_name,
+    'placement' => $placement,
+    'site_source_name' => $site_source_name,
+    'status' => 'PageView',
+    'fbclid' => $fbclid,
+    'invite_code' => $invite_link,
+    'created_at' => date('Y-m-d H:i:s'),
+    'client_ip_address' => $client_ip_address,
+    'client_user_agent' => $client_user_agent,
     'refer' => $client_referer,
+    'gclid' => $gclid,
 ])->execute();
 
 ?>
