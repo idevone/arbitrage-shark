f73e178c044f72e90814c46f684fec7297b31ef2
dkarputs <dkarputs@gmail.com>
2024-09-27 15:43:59 +0200

    fix referer getting

--- crm/controllers/RedirectController.php
+++ crm/controllers/RedirectController.php
@@ -42,7 +42,7 @@ class RedirectController extends Controller
                 $invite_code = $invite_link;
                 $client_ip_address = $_SERVER['REMOTE_ADDR'];
                 $client_user_agent = $_SERVER['HTTP_USER_AGENT'];
-                $client_referer = $_SERVER['HTTP_REFERER'] ?? null;
+                $client_referer = Yii::$app->request->referrer;
 
                 Yii::$app->db->createCommand()->insert('audience', [
                     'pixel_id' => $pixel_id,
